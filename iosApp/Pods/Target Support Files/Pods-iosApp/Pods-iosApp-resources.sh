ea117980d5ae213391506daea837c63ddf750a56
Ali Nasrabadi <nasrabadiam@cafebazaar.ir>
2023-06-28 12:06:02 +0330

    Use design system helpers in ui

--- iosApp/Pods/Target Support Files/Pods-iosApp/Pods-iosApp-resources.sh
+++ iosApp/Pods/Target Support Files/Pods-iosApp/Pods-iosApp-resources.sh
@@ -97,10 +97,10 @@ EOM
   esac
 }
 if [[ "$CONFIGURATION" == "Debug" ]]; then
-  install_resource "${PODS_CONFIGURATION_BUILD_DIR}/androidApp/LibresAndroidApp.bundle"
+  install_resource "${PODS_ROOT}/../../shared/src/commonMain/resources/MR"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
-  install_resource "${PODS_CONFIGURATION_BUILD_DIR}/androidApp/LibresAndroidApp.bundle"
+  install_resource "${PODS_ROOT}/../../shared/src/commonMain/resources/MR"
 fi
 
 mkdir -p "${TARGET_BUILD_DIR}/${UNLOCALIZED_RESOURCES_FOLDER_PATH}"
